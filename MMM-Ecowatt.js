08da8a33ca1a30b4037a33bd44700f02dd999817
Grena <grena@grenabox.fr>
2022-09-30 21:11:31 +0200

    Fix indents

--- MMM-Ecowatt.js
+++ MMM-Ecowatt.js
@@ -7,19 +7,19 @@
  */
 Module.register("MMM-Ecowatt", {
 
-    // Default module config.
-    defaults: {
-        updateInterval: 20 * 60 * 1000, // every 20 minutes
+	// Default module config.
+	defaults: {
+		updateInterval: 20 * 60 * 1000, // every 20 minutes
 		debug: 1,
-        apiBaseUrl: "https://digital.iservices.rte-france.com",
-        apiOAuthPath: "/token/oauth/",
-        apiSignalsPath: "/open_api/ecowatt/v4/signals", 
-        //apiSignalsPath: "/open_api/ecowatt/v4/sandbox/signals", //sandbox
-        showText: true,
-        showGraph: true,
-        signals: [],
-        hasData: false
-    },
+		apiBaseUrl: "https://digital.iservices.rte-france.com",
+		apiOAuthPath: "/token/oauth/",
+		apiSignalsPath: "/open_api/ecowatt/v4/signals", 
+		//apiSignalsPath: "/open_api/ecowatt/v4/sandbox/signals", //sandbox
+		showText: true,
+		showGraph: true,
+		signals: [],
+		hasData: false
+	},
 
 	getTemplate: function () {
 		return "MMM-Ecowatt.njk"
@@ -29,97 +29,97 @@ Module.register("MMM-Ecowatt", {
 		return this.config;
 	},
 
-    // Define required scripts.
-    getScripts: function() {
-        return ["moment.js"];
-    },
+	// Define required scripts.
+	getScripts: function() {
+		return ["moment.js"];
+	},
 
-    // Define required scripts.
-    getStyles: function() {
-        return ["MMM-Ecowatt.css"];
-    },
+	// Define required scripts.
+	getStyles: function() {
+		return ["MMM-Ecowatt.css"];
+	},
 
-    // Define start sequence.
-    start: function() {
-        Log.info("Starting module: " + this.name);
+	// Define start sequence.
+	start: function() {
+		Log.info("Starting module: " + this.name);
 
-        // Set locale.
-        moment.locale(config.language);
-        
-        this.loaded = false;
-        this.error = false;
-        this.errorDescription = "";
-        this.getSignals();
-        this.updateTimer = null;
-        this.systemp = "";
-    },
+		// Set locale.
+		moment.locale(config.language);
 
-    getSignals: function() {
-        if (this.config.debug === 1) {
+		this.loaded = false;
+		this.error = false;
+		this.errorDescription = "";
+		this.getSignals();
+		this.updateTimer = null;
+		this.systemp = "";
+	},
+
+	getSignals: function() {
+		if (this.config.debug === 1) {
 			Log.info("Ecowatt: Getting info.");
 		}
 		this.sendSocketNotification("GET_ECOWATT", this.config);
-    },
+	},
 
-    /* processSignals(data)
-     * Uses the received data to set the various values.
-     *
-     * argument data object - Signals data received form ecowatt.
-     */
-    processSignals: function(data) {   
+	/* processSignals(data)
+	 * Uses the received data to set the various values.
+	 *
+	 * argument data object - Signals data received form ecowatt.
+	 */
+	processSignals: function(data) {
 		if (this.config.debug === 1) {
 			Log.info('ECOWATT processSignals data : ');
 			Log.info(data);
 		} 
-		
-        if (data) {
-            this.config.hasData = data.signals != null && data.signals.length > 0;      
-            
-            if (this.config.hasData) {
-                // Graph data
-                this.config.signals = data.signals.sort(function(a,b){
-                    return new Date(a.jour) - new Date(b.jour);
-                });
-                var momentToday = moment(new Date());
-                var momentTomorrow = moment(new Date()).add(1, 'day');
-                this.config.signals.forEach(signal => {                  
-                    var momentDay = moment(signal.jour);
-                    if (momentDay.startOf('day').diff((momentToday.startOf('day')), 'days') == 0) {
-                        signal.displayDay = "aujourd'hui"; 
-                    } else if (momentDay.startOf('day').diff((momentTomorrow).startOf('day'), 'days') == 0) {
-                        signal.displayDay = "demain"; 
-                    } else {
-                        signal.displayDay = momentDay.format('dddd');
-                    }
-                });
 
-                if (this.config.days) {
-                    this.config.signals.length = this.config.days;
-                }
-            }
+		if (data) {
+			this.config.hasData = data.signals != null && data.signals.length > 0;
 
-            this.updateDom(this.config.animationSpeed);
-            
+			if (this.config.hasData) {
+				// Graph data
+				this.config.signals = data.signals.sort(function(a,b){
+					return new Date(a.jour) - new Date(b.jour);
+				});
+				var momentToday = moment(new Date());
+				var momentTomorrow = moment(new Date()).add(1, 'day');
+				this.config.signals.forEach(signal => {
+					var momentDay = moment(signal.jour);
+					if (momentDay.startOf('day').diff((momentToday.startOf('day')), 'days') == 0) {
+						signal.displayDay = "aujourd'hui"; 
+					} else if (momentDay.startOf('day').diff((momentTomorrow).startOf('day'), 'days') == 0) {
+						signal.displayDay = "demain"; 
+					} else {
+						signal.displayDay = momentDay.format('dddd');
+					}
+				});
+
+				if (this.config.days) {
+					this.config.signals.length = this.config.days;
+				}
+			}
+
+			this.updateDom(this.config.animationSpeed);
+
 		} else {
-            this.config.hasData = false;
-        }
-    },
+			this.config.hasData = false;
+		}
+	},
 
-    socketNotificationReceived: function(notification, payload) {
-        var self = this;
+	socketNotificationReceived: function(notification, payload) {
+		var self = this;
 
-        if (this.config.debug === 1) {
+		if (this.config.debug === 1) {
 			Log.info('ECOWATT received ' + notification);
 		}
 
-        if (notification === "ECOWATT") {
-            if (this.config.debug === 1) {
+		if (notification === "ECOWATT") {
+			if (this.config.debug === 1) {
 				Log.info('received ECOWATT');
 				Log.info(payload);
 			}
-            self.processSignals(payload);
-        }
+			self.processSignals(payload);
+		}
 
-    }
+	}
 
 });
